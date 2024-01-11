27841da9e2d42d79fde0c686bd7ea5e4633ba334
Gustavobvs1 <gustavobvs06@gmail.com>
2024-01-11 03:16:34 -0300

    create getUnits service

---
+++ src/app/services/get-units.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UnitsResponse } from '../types/units-response.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetUnitsService {
+  constructor(private httpClient: HttpClient) {}
+
+  readonly apiUrl =
+    'https://test-frontend-developer.s3.amazonaws.com/data/locations.json';
+
+  getAllUnits(): Observable<UnitsResponse> {
+    return this.httpClient.get<UnitsResponse>(this.apiUrl);
+  }
+}
