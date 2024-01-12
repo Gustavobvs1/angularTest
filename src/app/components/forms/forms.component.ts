ac64ad8bb48bd5e6c237311839c96bab7ea7d7da
Gustavobvs1 <gustavobvs06@gmail.com>
2024-01-11 22:10:06 -0300

    card done

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FilterUnitsService } from 'src/app/services/filter-units.service';
 import { GetUnitsService } from 'src/app/services/get-units.service';
@@ -10,6 +10,7 @@ import { Location } from 'src/app/types/location.interface';
   styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent implements OnInit {
+  @Output() submitEvent = new EventEmitter();
   results: Location[] = [];
   filteredResults: Location[] = [];
   formGroup!: FormGroup;
@@ -21,8 +22,8 @@ export class FormsComponent implements OnInit {
   ) {}
   ngOnInit(): void {
     this.unitService.getAllUnits().subscribe((data) => {
-      this.results = data.locations;
-      this.filteredResults = data.locations;
+      this.results = data;
+      this.filteredResults = data;
     });
 
     this.formGroup = this.formBuilder.group({
@@ -38,6 +39,9 @@ export class FormsComponent implements OnInit {
       showClosed,
       hour
     );
+    this.unitService.setFilteredUnits(this.filteredResults);
+
+    this.submitEvent.emit();
   }
 
   onClean(): void {
--- src/app/services/get-units.service.ts
+++ src/app/services/get-units.service.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { UnitsResponse } from '../types/units-response.interface';
+import { Location } from '../types/location.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GetUnitsService {
-  constructor(private httpClient: HttpClient) {}
+  private allUnitsSubject: BehaviorSubject<Location[]> = new BehaviorSubject<
+    Location[]
+  >([]);
+  private allUnits$: Observable<Location[]> =
+    this.allUnitsSubject.asObservable();
+  private filteredUnits: Location[] = [];
 
   readonly apiUrl =
     'https://test-frontend-developer.s3.amazonaws.com/data/locations.json';
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get<UnitsResponse>(this.apiUrl).subscribe((data) => {
+      this.allUnitsSubject.next(data.locations);
+      this.filteredUnits = data.locations;
+    });
+  }
+  getAllUnits(): Observable<Location[]> {
+    return this.allUnits$;
+  }
 
-  getAllUnits(): Observable<UnitsResponse> {
-    return this.httpClient.get<UnitsResponse>(this.apiUrl);
+  getFilteredUnits() {
+    return this.filteredUnits;
+  }
+
+  setFilteredUnits(value: Location[]) {
+    this.filteredUnits = value;
   }
 }
